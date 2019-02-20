6430a05c166ae73a6dee016adeaa0649c4486f62
ranuseh <rosalyn.anuseh@gmail.com>
2019-02-20 15:58:44 -0800

    working on wave 3

--- adagram.rb
+++ adagram.rb
@@ -37,9 +37,50 @@ LETTERS = Set["A" "A" "A" "A" "A" "A" "A" "A" "A" "B" "B" "C" "C" "D" "D" D D E 
 
 # # Use sets. Then you can use set.subset?. Example:
 
-require 'set'
+# require 'set'
 
-a1 = Set['A','B','C']
-a2 = Set['A','B','C','S','W','S']
+# a1 = Set['A','B','C']
+# a2 = Set['A','B','C','S','W','S']
 
-puts a1.subset?(a2)
+# puts a1.subset?(a2)
+
+def store_word (word)
+  score_chart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
+  }
+  score = 0
+
+  score_chart.each do |key, value|
+    if word.include?(key.to_s)
+      score += value
+    end
+  end
+  return score
+end
+
+puts store_word('HELLO')
