513268aada63a8aabe5d8cca9fd1cec4c96392aa
Carla Bosco <carlabosco@gmail.com>
2019-02-20 13:41:45 -0800

    First steps

--- adagram.rb
+++ adagram.rb
@@ -9,13 +9,37 @@
 # Invoking this method should not change the pool of letters
 # Imagine that the user returns their hand to the pool before drawing new letters
 
-LETTERS = %W(A A A A A A A A A B B C C D D D D E E E E E E E E E E E E F F G G G H H I 
+require 'set'
+
+LETTERS = Set["A" "A" "A" "A" "A" "A" "A" "A" "A" "B" "B" "C" "C" "D" "D" D D E E E E E E E E E E E E F F G G G H H I 
   I I I I I I I I J K L L L L M M N N N N N N O O O O O O O O P P Q R R R R R R S S S S T T T
   T T T U U U U V V W W X Y Y Z)
 
-def draw_letters
-  sample = LETTERS.sample(10)
-  letters_in_hand = sample.each_with_object(Hash.new(0)){|key,hash| hash[key] += 1}
-end
+# def draw_letters
+#   sample = Set["A", "S", "D", "E"]
+#   puts sample
+#   # letters_in_hand = sample.each_with_object(Hash.new(0)){|key,hash| hash[key] += 1}
+# end
 
-puts draw_letters
+# print draw_letters
+
+
+# def uses_available_letters? (input, letters_in_hand)
+#   draw_letters.subset?(input)
+#   # returns true if letters in hand = input
+#   # returns false if 1 letter in input is not present in the letters in hand
+#   # OR more letters in hand than there is in the input
+#   print user_input
+# end
+
+# puts uses_available_letters?(Set["A","S","W","E","D","S"], draw_letters)
+
+
+# # Use sets. Then you can use set.subset?. Example:
+
+require 'set'
+
+a1 = Set['A','B','C']
+a2 = Set['A','B','C','S','W','S']
+
+puts a1.subset?(a2)
