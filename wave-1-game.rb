68eea4f12c85b3a9e0b66442575baad2dbf699a9
Karla Guadron <ksguadron@gmail.com>
2019-02-19 16:12:35 -0800

    Added a loop in draw_letters method that prevents overdrawing letters. Removed method parameter

--- wave-1-game.rb
+++ wave-1-game.rb
@@ -15,20 +15,22 @@ def run_game
   display_drawn_letters(draw_letters)
 end
 
-available_letters = {"a" => 9, "b" => 2, "c" => 2, "d" => 4, "e" => 12, "f" => 2, "g" => 3, "h" => 2, 
-                     "i" => 9, "j" => 1, "k" => 1, "l" => 4, "m" => 2, "n" => 6, "o" => 8, "p" => 2, 
-                     "q" => 1, "r" => 6, "s" => 4, "t" => 6, "u" => 4, "v" => 2, "w" => 2, "x" => 1, 
-                     "y" => 2, "z" => 1}
-
-def draw_letters(hash)
+def draw_letters
+  available_letters = {"a" => 9, "b" => 2, "c" => 2, "d" => 4, "e" => 12, "f" => 2, "g" => 3, "h" => 2,
+    "i" => 9, "j" => 1, "k" => 1, "l" => 4, "m" => 2, "n" => 6, "o" => 8, "p" => 2,
+    "q" => 1, "r" => 6, "s" => 4, "t" => 6, "u" => 4, "v" => 2, "w" => 2, "x" => 1,
+    "y" => 2, "z" => 1}
   selected_letters = []
-  10.times do 
-    letter = hash.keys.sample
+  10.times do
+    letter = available_letters.keys.sample
+    while available_letters[letter] == 0
+      letter = available_letters.keys.sample
+    end
     selected_letters.push(letter)
-    hash[letter] = hash[letter] - 1
+    available_letters[letter] = available_letters[letter] - 1
   end
   return selected_letters
 end
 
 # puts selected_letters
-draw_letters(available_letters)
+puts draw_letters(available_letters)
