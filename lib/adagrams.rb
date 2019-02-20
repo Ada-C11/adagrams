b70dd109900d5420a1ec7170a9b6a99b4caed1cb
Nara <naraa_520@yahoo.com>
2019-02-20 15:45:50 -0800

    Updated uses available letters method

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,11 +1,11 @@
-require "pry"
+require 'pry'
 
 def draw_letters
   letter_pool = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
   letter_array = []
   letter_pool.each do |letter, quantity|
     quantity.times do
-      letter_array << letter.to_s
+      letter_array << letter.to_s.downcase
     end
   end
   return letter_array.sample(10)
@@ -14,15 +14,16 @@ end
 def uses_available_letters?(input, letters_in_hand)
   size = 0
 
-  input.split(//).each do |letter|
+  input.downcase.split(//).each_with_index do |letter, index|
     if letters_in_hand.include?(letter)
       size += 1
+      letters_in_hand.delete(index)
     end
   end
-
+  
   if size == input.length
     return true
-  else
+  elsif size != input.length
     return false
   end
 end
