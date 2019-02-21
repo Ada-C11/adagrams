1027f0b42d0207a827944000498b98cc2a75449f
Maria Wissler <wmcarmelina@gmail.com>
2019-02-21 15:10:21 -0800

    latest work

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,49 +1,88 @@
-POOL = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", 
- "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", 
- "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", 
- "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K",
- "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N",
- "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", 
- "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", 
- "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", 
- "V", "V", "W", "W", "X", "Y", "Y", "Z"]
+draw_letters = %w[A A A A A A A A A B B
+  C C D D D D E E E E E E
+  E E E E E E F F G G G H
+  H I I I I I I I I I J K
+  L L L L M M N N N N N N
+  O O O O O O O O P P Q
+  R R R R R R S S S S
+  T T T T T T U U U U
+  V V W W X Y Y Z].freeze
 
-# add welcome message etc 
-def welcome_message
-    puts "Welcome Message"
-
-
-def draw_letters(array)
-array.shuffle
-return list = array.sample(10)
-end 
-      
-list = draw_letters(POOL)
-puts "This are the letter you have drawn #{list.join(', ')}"
-
-# Wave 2 
-puts "enter a word" 
-word = gets.chomp.upcase  
-
-#Returns true if every letter in the input word is available
-# (in the right quantities) in the letters_in_hand
-#Returns false if not; if there is a letter in input 
-#that is not present in the letters_in_hand or has too 
-#much of compared to the letters_in_hand
-def uses_available_letters?(input, letters_in_hand)
- chars = input.split("")
- puts "#{chars}"
- chars.each do |item|
-    if letters_in_hand.include?(item) == false
-        puts "not working"
-       return false
-    elsif letters_in_hand.include?(item) == true
-       letters_in_hand.delete(item)
-
-end
-end
-puts "yes"
-return true
+def display_welcome_message
+puts "Welcome to Adagrams!"
 end
 
-uses_available_letters?(word, list)    
+def display_drawn_letters(letters)
+letters.shuffle
+letters = letters.sample(10)
+puts "You have drawn the letters:"
+puts letters.join(', ')
+end
+
+def display_game_instructions
+puts "Please input your submission for the longest anagram you can come up with"
+get_user_input 
+end
+
+def uses_available_letters?(user_input_word, letter_bank)
+user_input_word.each_char do |letter|
+if user_input_word.include?(letter_bank) == false
+puts "You entered in a word that contains characters not in the letter bank"
+display_game_instructions
+end
+end
+
+SCORE_VALUE = { A: 1 ,B: 3, C: 3, D: 2, E: 1, F: 4,G: 2, H: 4, I: 1, J: 8, K: 5 ,L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10 , R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10 }.freeze
+def display_score(input_word)
+scored = 0
+input_word.each_char do |letter|
+scored += SCORE_VALUE[letter.to_sym].to_i
+end
+puts scored 
+end
+
+def display_retry_instructions
+puts "Should we play another round?"
+puts "Enter y to replay"
+end
+
+def display_goodbye_message
+puts "Thanks for playing Adagrams!"
+end
+
+def get_user_input
+word = gets.chomp.upcase
+end
+
+def run_game
+display_welcome_message
+
+should_continue = true
+
+while should_continue
+puts "Let's draw 10 letters from the letter pool..."
+
+letter_bank = @draw_letters
+display_drawn_letters(letter_bank)
+
+display_game_instructions
+
+user_input_word = get_user_input
+
+while ( !(uses_available_letters?(user_input_word, letter_bank)) )
+display_needs_valid_input_message
+user_input_word = get_user_input
+end
+
+score = display_score(user_input_word)
+
+puts score
+
+display_retry_instructions
+should_continue = get_user_input == "y"
+end
+
+display_goodbye_message
+end
+
+run_game
