7f0c99a2bbcc581aad93dbef41b6485d33a2bf79
Amy Wyatt <amy.wyatt12@outlook.com>
2019-02-22 14:53:11 -0800

    Refactored draw letters

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,15 +1,17 @@
 def draw_letters
-  letterset =
-    ["A", "A", "A", "A", "A", "A", "A", "A", "A",
-     "B", "B", "C", "C", "D", "D", "D", "D",
-     "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
-     "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I",
-     "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N",
-     "O", "O", "O", "O", "O", "O", "O", "O", "P", "P",
-     "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S",
-     "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V",
-     "W", "W", "X", "Y", "Y", "Z"]
-
+  letter_distribution =
+    {
+      A: 9, B: 2, C: 2, D: 4, E: 12, F: 2,
+      G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2,
+      N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6,
+      U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1,
+    }
+  letterset = []
+  letter_distribution.each do |letter, value|
+    value.times do
+      letterset << letter.to_s
+    end
+  end
   hand = letterset.sample(10)
   return hand
 end
@@ -21,15 +23,16 @@ def uses_available_letters?(input, letters_in_hand)
 end
 
 def score_word(word)
-  letter_value = {
-    "AEIOULNRST" => 1,
-    "DG" => 2,
-    "BCMP" => 3,
-    "FHVWY" => 4,
-    "K" => 5,
-    "JX" => 8,
-    "QZ" => 10,
-  }
+  letter_value =
+    {
+      "AEIOULNRST" => 1,
+      "DG" => 2,
+      "BCMP" => 3,
+      "FHVWY" => 4,
+      "K" => 5,
+      "JX" => 8,
+      "QZ" => 10,
+    }
   total = 0
   letter_score = 0
 
