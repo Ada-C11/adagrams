0320bab2faae87374a69fab6cc3ebca1c84763c1
Nara <naraa_520@yahoo.com>
2019-02-22 15:09:47 -0800

    Cleaned up the code

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 def draw_letters
   letter_pool = { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 }
   letter_array = []
@@ -21,6 +19,7 @@ def uses_available_letters?(input, letters_in_hand)
       letters_in_hand.delete(index)
     end
   end
+
   if size == input_array.length
     return true
   else
@@ -74,7 +73,7 @@ def highest_score_from(words)
       max_words[:score] = v
     end
   end
-puts max_words
+
   best_word = {}
 
   min_length = 10
