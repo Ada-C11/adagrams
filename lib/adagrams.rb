91e169bdd5b3be7da52ef2f54b06cbfb9368bc9a
Amy Phung <phung.amy85@gmail.com>
2019-02-22 15:05:11 -0800

    removed erroneous comments

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,7 +1,5 @@
 require "pry"
 
-# populating letter pool
-
 def draw_letters
   letter_distribution = {
     "a" => 9,
@@ -125,6 +123,5 @@ def highest_score_from(words)
       end
     end
   end
-  # puts "The word with the highest score is: #{winning_word}"
   return winning_word
 end
