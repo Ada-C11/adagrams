c38c18d92cbffff9e21e9990f6eb09b5cbd2ad9e
Maria Wissler <wmcarmelina@gmail.com>
2019-02-21 23:35:37 -0800

    latest work

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,4 +1,4 @@
-draw_letters = %w[A A A A A A A A A B B
+DRAW_LETTERS = %w[A A A A A A A A A B B
   C C D D D D E E E E E E
   E E E E E E F F G G G H
   H I I I I I I I I I J K
@@ -6,7 +6,7 @@ draw_letters = %w[A A A A A A A A A B B
   O O O O O O O O P P Q
   R R R R R R S S S S
   T T T T T T U U U U
-  V V W W X Y Y Z].freeze
+  V V W W X Y Y Z]
 
 def display_welcome_message
 puts "Welcome to Adagrams!"
@@ -14,31 +14,38 @@ end
 
 def display_drawn_letters(letters)
 letters.shuffle
-letters = letters.sample(10)
+letters_in_hand = letters.sample(10)
 puts "You have drawn the letters:"
-puts letters.join(', ')
+puts letters_in_hand.join(', ')
+return letters_in_hand
 end
 
 def display_game_instructions
 puts "Please input your submission for the longest anagram you can come up with"
-get_user_input 
+result = get_user_input()
+return result 
 end
 
-def uses_available_letters?(user_input_word, letter_bank)
+def uses_available_letters?(user_input_word, letters_in_hand)
 user_input_word.each_char do |letter|
-if user_input_word.include?(letter_bank) == false
+if !letters_in_hand.include?(letter)
 puts "You entered in a word that contains characters not in the letter bank"
 display_game_instructions
+else    
+letters_in_hand.delete(letter.size)
 end
+end
+display_score(user_input_word)
 end
 
 SCORE_VALUE = { A: 1 ,B: 3, C: 3, D: 2, E: 1, F: 4,G: 2, H: 4, I: 1, J: 8, K: 5 ,L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10 , R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10 }.freeze
-def display_score(input_word)
-scored = 0
-input_word.each_char do |letter|
+
+def display_score(user_input_word)
+scored = 0 
+user_input_word.each_char do |letter|
 scored += SCORE_VALUE[letter.to_sym].to_i
 end
-puts scored 
+return scored 
 end
 
 def display_retry_instructions
@@ -52,6 +59,7 @@ end
 
 def get_user_input
 word = gets.chomp.upcase
+return word
 end
 
 def run_game
@@ -62,14 +70,12 @@ should_continue = true
 while should_continue
 puts "Let's draw 10 letters from the letter pool..."
 
-letter_bank = @draw_letters
-display_drawn_letters(letter_bank)
+letters = DRAW_LETTERS
+display_drawn_letters(letters)
 
-display_game_instructions
+user_input_word = display_game_instructions()
 
-user_input_word = get_user_input
-
-while ( !(uses_available_letters?(user_input_word, letter_bank)) )
+while ( !(uses_available_letters?(user_input_word, letters)) )
 display_needs_valid_input_message
 user_input_word = get_user_input
 end
