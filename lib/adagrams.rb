d666c12d983af91483f3cc0826ae03675f5b40c5
qqdipps <sdipp007@gmail.com>
2019-02-22 09:40:50 -0800

    added score_word method, tests ok

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -22,3 +22,18 @@ def uses_available_letters?(input, letters_in_hand)
   end
   return true
 end
+
+def score_word(word)
+  score_chart = Hash.new(0)
+  score_chart = { a: 1, e: 1, i: 1, o: 1, u: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, l: 1,
+                  b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 }
+  score = 0
+  word.downcase.each_char do |char|
+    # p score_chart[char.to_sym]
+    score += score_chart[char.to_sym]
+  end
+  if word.length == 7 || word.length == 8 || word.length == 9 || word.length == 10
+    score += 8
+  end
+  return score
+end
