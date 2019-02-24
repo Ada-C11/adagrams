f6888d18f74ed3480b1abcb2afc78242080cc19a
Maria Wissler <wmcarmelina@gmail.com>
2019-02-23 20:41:57 -0800

    corrected bug, pass all test

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,69 +1,75 @@
-
 def draw_letters
   pool_letters = %w[A A A A A A A A A B B
-    C C D D D D E E E E E E
-    E E E E E E F F G G G H
-    H I I I I I I I I I J K
-    L L L L M M N N N N N N
-    O O O O O O O O P P Q
-    R R R R R R S S S S
-    T T T T T T U U U U
-    V V W W X Y Y Z]
+                    C C D D D D E E E E E E
+                    E E E E E E F F G G G H
+                    H I I I I I I I I I J K
+                    L L L L M M N N N N N N
+                    O O O O O O O O P P Q
+                    R R R R R R S S S S
+                    T T T T T T U U U U
+                    V V W W X Y Y Z]
 
-letters = pool_letters 
-letters.shuffle
-letters_in_hand = letters.sample(10)
-return letters_in_hand
+  letters = pool_letters
+  letters.shuffle
+  letters_in_hand = letters.sample(10)
+  letters_in_hand
 end
-
 
 def uses_available_letters?(input, letters_in_hand)
   input.each_char do |letter|
     index = letters_in_hand.index(letter.upcase)
     if index.nil?
-        return false
+      return false
     else
-        letters_in_hand.delete_at(index)
+      letters_in_hand.delete_at(index)
     end
   end
-return true
+  true
 end
 
 def score_word(word)
-score_value = { A: 1, B: 3, C: 3, D: 2, E: 1, F: 4,G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10 , R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10 }
-scored = 0 
-word = word.upcase
-word.each_char do |letter|
-scored += score_value[letter.to_sym].to_i
-end
-if word.length > 6 
-  scored += 8
-end
-return scored 
+  score_value = { A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10 }
+  scored = 0
+  word = word.upcase
+  word.each_char do |letter|
+    scored += score_value[letter.to_sym].to_i
+  end
+  scored += 8 if word.length > 6
+  scored
 end
 
 def highest_score_from(words)
   scores = []
   words.each do |word|
-  score = score_word(word.upcase)
-  scores << score
+    score = score_word(word.upcase)
+    scores << score
   end
 
   scores_hash = Hash[words.zip scores]
-  winner_score = scores_hash.select { |key, value|  value == scores_hash.values.max }
-  
-  winner = ""
-    winner_score.each do |key|
+  puts "scores_hash #{scores_hash}"
+  winner_score = scores_hash.select { |_key, value| value == scores_hash.values.max }
+  puts "winner_score #{winner_score}"
+  puts "winner_score.length #{winner_score.length}"
+
+  long_words = []
+  winner_score.each do |key, _value|
     if key.length == 10
-      winner = key
-      break
-    else
-       winner = winner_score.min_by {|key| key.length }
+      long_words << key
     end
   end
-  winner_values = {}
-  winner_values[:word] =  winner[0]
-  winner_values[:score] = scores.max
-  return winner_values
-end
 
+  winner = ""
+  if long_words.length == 1
+    winner = long_words[0]
+  elsif long_words.length > 1
+    winner = long_words[0]
+  else
+    short_words = winner_score.min_by { |key, _value| key.length }
+    winner = short_words[0]
+  end
+
+  winner_values = {}
+  winner_values[:word] =  winner
+  winner_values[:score] = scores.max
+  winner_values
+end
