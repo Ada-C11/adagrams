661fcdbf75639590b023f4201de0737a6f49a0bc
Mello-Cello <celloelle@gmail.com>
2019-02-23 16:10:14 -0800

    Completed code for Adagrams assignment. This code still contains helpful testing with staarfish for troubleshooting. Next commit will clean that up for instructor review.

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -32,7 +32,6 @@ letter_quantities.each do |letter, quantity|
     $letter_pool << letter.to_s
   end
 end
-# p $letter_pool
 
 $letter_scores = {
   1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
@@ -50,25 +49,21 @@ def draw_letters
   return letters_in_hand
 end
 
-# p draw_letters
-
 # Wave 2
 def uses_available_letters?(input, letters_in_hand)
   input = input.upcase.split("")
-  #   input_copy = []
-  #   input.each do |i|
-  #     input_copy << i
-  #   end
+  # Make a copy of the input array and keep the original 
+  # array unchanged. The original input needs to remain in tact so
+  # that Ruby can continue to iterate through it the 
+  # uses_available_letters methods.
   input_copy = input.dup
-
-  # ^^^^^^^ SHOW THIS TO SOPHERY ^^^^^^^
 
   p input
 
   # Check if letters in the input are all in the letters_in_hand array
-  i = 1
+  loop_count = 1
   input.each_with_index do |letter, input_index|
-    puts "Loop count: #{i}"
+    puts "Loop count: #{loop_count}"
     if letters_in_hand.include?(letter)
       hand_index = 0
       letters_in_hand.each_with_index do |letter_in_hand, index|
@@ -81,27 +76,28 @@ def uses_available_letters?(input, letters_in_hand)
       puts "This letter is: #{letter}"
       puts "Letters in hand before delete are: #{letters_in_hand}"
       letters_in_hand.delete_at(hand_index)
-      # ^^^^^ SHOW THIS TO SOPHERY ^^^^^^^
-      # try foo.delete_at(foo.index(4))
-      # in order to only delete one instance of a letter
-      # such as the S, when there are two S's in starfish
-      # maybe use this in one or both places where we use .delete
+
+      i = 0
+      input_copy.each_with_index do |input_copy_letter, input_copy_index|
+        if input_copy_letter == letter
+          i = input_copy_index
+          break
+        end
+      end
       puts "Letters in hand after delete are: #{letters_in_hand}"
       puts "Letters in input_copy before delete are: #{input_copy}"
-      input_copy.delete_at(0)
+      input_copy.delete_at(i)
       puts "Letters in input_copy after delete are: #{input_copy}"
     end
-    i += 1
+    loop_count += 1
   end
+
   if input_copy.length == 0
     return true
   else
     return false
   end
 end
-
-# letters_in_hand = draw_letters
-# p uses_available_letters?("hi", letters_in_hand)
 
 # This method calculates a score for the word the user submits.
 def score_word(word)
@@ -110,10 +106,8 @@ def score_word(word)
     total_score += 8
   end
   this_word = word.upcase.split("")
-  # p this_word
   this_word.each do |this_letter|
     $letter_scores.each do |score, letter_array|
-      # p $letter_scores
       if letter_array.include?(this_letter)
         total_score += score
       end
@@ -122,10 +116,7 @@ def score_word(word)
   return total_score
 end
 
-# ^^^^^^ If we have time, change input to global variable. Assign word in score_word = input. Update as needed. ^^^^^^^^^^^^^^^
-# FIX THE LOOP COUNT SKIPPING IF THERE ARE MULTIPLE INSTANCES OF THE SAME LETTER (example: staarfish, set includes only one A)
-
-# #["H", "S", "I", "F", "R", "A", "S", "T"]
+# ["H", "S", "I", "F", "R", "A", "S", "T"]
 
 def highest_score_from(words)
   words_scores_array = []
@@ -148,23 +139,28 @@ def highest_score_from(words)
       winning_words << this_word_score[:word]
     end
   end
-  # if there's a tie
+  # Detect and handle ties
   if winning_words.length > 1
     winning_words.each do |this_word|
       if this_word.length > 9
         winning_word[:word] = this_word
+        break
       else
         winning_word[:word] = winning_words.min_by do |nested_word|
           nested_word.length
         end
       end
     end
-    winning_word[:score] = max_score.to_i
+  else
+    winning_word[:word] = winning_words[0]
   end
+
+  winning_word[:score] = max_score.to_i
+  return winning_word
 end
 
 # letters_in_hand = ["H", "S", "I", "F", "R", "A", "S", "T", "W"]
-# p uses_available_letters?("starfish", letters_in_hand)
+# p uses_available_letters?("staarfish", letters_in_hand)
 # score = score_word("starfish")
 
 # puts winning_words
